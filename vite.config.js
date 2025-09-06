0eed025d476c31b3c433a498dcea98f8d50cb85a
Agus <agustin_414@hotmail.com>
2025-09-05 22:28:25 -0300

    vite.config.js updated

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+// Este archivo configura Vite para que los 
+// archivos estáticos del proyecto se sirvan desde la ruta '/js-vite-blackjack/'.
+
+// Es útil cuando el proyecto se despliega 
+// en un subdirectorio, por ejemplo en GitHub Pages.
+
+// La función defineConfig permite definir 
+// opciones de configuración para el entorno de construcción y desarrollo de Vite.
+
+
+import { defineConfig } from "vite";
+
+export default defineConfig({
+    base: '/js-vite-blackjack/',
+});
